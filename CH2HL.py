3b531098a596f33adb0c1e18baa685577df519e6
sean54017 <seanchen54017@gmail.com>
2019-12-04 12:58:56 +0800

    setGPIO5

---
+++ CH2HL.py
@@ -0,0 +1,1 @@
+[{"id":"9e6863ea.7c091","type":"http in","z":"8758a379.39add","name":"set GPIO5","url":"/setGPIO5","method":"get","upload":false,"swaggerDoc":"","x":210,"y":220,"wires":[["3f64fea9.a5df72","392a5a0e.69cab6"]]}]
